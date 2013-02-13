06f73e9fcdbc358b2d3331f293f6022ffab690fd
mcandre <andrew.pennebaker@gmail.com>
2013-02-13 09:17:08 -0500

    debugging for_all

--- example.rb
+++ example.rb
@@ -2,7 +2,43 @@
 
 require "rubycheck"
 
-puts "Random int: #{gen_int}"
-puts "Random byte: #{gen_byte}"
-puts "Random char: #{gen_char}"
-puts "Random string: #{gen_str}"
+module RubyCheck
+	def self.gen_even
+		i = gen_int
+
+		if i % 2 == 0
+			i
+		else
+			i + 1
+		end
+	end
+end
+
+propEven = :even?
+
+# Should fail.
+RubyCheck::for_all(propEven, [:gen_int])
+
+# Should pass.
+RubyCheck::for_all(propEven, [:gen_even])
+
+class String
+	def reversible?
+		this == reverse.reverse
+	end
+end
+
+module RubyCheck
+	def self.gen_palindrome
+		s = gen_str
+		s + s.reverse
+	end
+end
+
+propReversible = :reversible?
+
+# Should fail.
+RubyCheck::for_all(propReversible, [:gen_str])
+
+# Should pass.
+RubyCheck::for_all(propReversible, [:gen_palindrome])
--- lib/rubycheck.rb
+++ lib/rubycheck.rb
@@ -1,34 +1,34 @@
-# Returns a random integer from 0 to 10^10 - 1.
-def gen_int
-	Random::rand(10e10).to_i
-end
+module RubyCheck
+	# Returns a random integer from 0 to 10^10 - 1.
+	def self.gen_int
+		Random::rand(10e10).to_i
+	end
 
-# Returns a random byte.
-def gen_byte
-	gen_int % 256
-end
+	# Returns a random byte.
+	def self.gen_byte
+		gen_int % 256
+	end
 
-# Returns a random ASCII character.
-def gen_char
-	(gen_int % 128).chr
-end
+	# Returns a random ASCII character.
+	def self.gen_char
+		(gen_int % 128).chr
+	end
 
-# Returning a random array given a value generator.
-def gen_array(gen)
-	len = gen_int % 100
+	# Returning a random array given a value generator.
+	def self.gen_array(gen_sym)
+		len = gen_int % 100
 
-	0.upto(len).collect { |i| gen.call }
-end
+		0.upto(len).collect { |i| RubyCheck.send(gen_sym) }
+	end
 
-# Returns a random string.
-def gen_str
-	gen_char_lambda = Proc.new { gen_char }
+	# Returns a random string.
+	def self.gen_str
+		gen_array(:gen_char).join("")
+	end
 
-	gen_array(gen_char_lambda).join("")
-end
-
-# Evaluates property over 100 random unit tests,
-# with argument values specified in generators.
-def for_all(property, generators)
-	# ...
+	# Evaluates property over 100 random unit tests,
+	# with argument values specified in generators.
+	def self.for_all(property, gen_syms)
+		# ...
+	end
 end
