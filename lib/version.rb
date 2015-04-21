c736f567c36187e87e374259e7714323601be45b
Andrew Pennebaker <andrew.pennebaker@networkedinsights.com>
2015-04-21 15:11:13 -0500

    bump to v0.0.8

--- lib/version.rb
+++ lib/version.rb
@@ -6,5 +6,5 @@ module RubyCheck
   # <i>VERSION</i> is defined once, available to gemspec during packaging,
   # and available programmatically to Ruby code.
   #
-  VERSION = '0.0.7'
+  VERSION = '0.0.8'
 end
